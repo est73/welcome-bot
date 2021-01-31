4f298d21f825acad3f62d9401fbb3044ea626e23
Brian Ross <brianross@outlook.com>
2021-01-31 00:04:24 -0800

    add logging

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ class WelcomeBot(discord.Client):
         self.invites = []
         self.logger = logging.getLogger('discord')
         self.logger.setLevel(logging.DEBUG)
-        self.handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+        self.handler = logging.FileHandler(filename='welcome-bot.log', encoding='utf-8', mode='w')
         self.handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
         self.logger.addHandler(self.handler)
 
